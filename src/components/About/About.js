82c0e3c521db82928eab4168d5c15fae24fbd3a1
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-05-01 18:41:03 +0100

    imported react

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,14 @@
+import React from "react";
 import styles from "./About.module.css";
 
 function About() {
     return (
         <div className={styles.wrapper}>
             <h2>About</h2>
-            <p>Mallards are a very good duck. They are so pretty and they go quack very loud</p>
+            <p>
+                Mallards are a very good duck. They are so pretty and they go
+                quack very loud
+            </p>
         </div>
     );
 }
