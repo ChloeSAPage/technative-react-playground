bf8b877fb6a90c789ed9c4531fbe083114a4f983
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-05-07 17:01:18 +0100

    added ability to click through duck images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Home.module.css";
 import { DUCKS } from "../../assets/data";
 
+function PrevButton({ index, previous }) {
+    return <button onClick={previous}>Previous</button>;
+}
+
+function NextButton({ index, next }) {
+    return <button onClick={next}>Next</button>;
+}
+
 function Home() {
+    const [index, setIndex] = useState(0);
+
+    function previous() {
+        if (index === 0) {
+            setIndex(DUCKS.length - 1);
+        } else {
+            setIndex(index - 1);
+        }
+    }
+
+    function next() {
+        if (index === DUCKS.length - 1) {
+            setIndex(0);
+        } else {
+            setIndex(index + 1);
+        }
+    }
+
     return (
         <div className={styles.wrapper}>
             <h2>Home</h2>
-            <img
-                src={DUCKS[2]}
-                alt="Mallard"
-            />
+            <img src={DUCKS[index]} alt="Mallard" className={styles.img} />
+            <div>
+                <PrevButton index={index} previous={previous} />
+                <NextButton index={index} next={next} />
+            </div>
         </div>
     );
 }
