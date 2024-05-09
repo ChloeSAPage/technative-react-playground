69fbc8333273dd9d2b4f2ee97b353024a7cda91a
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-05-09 15:23:26 +0100

    updated formatting

--- src/components/info/Form/Form.jsx
+++ src/components/info/Form/Form.jsx
@@ -3,19 +3,23 @@ import { useState } from "react";
 import styles from "./form.module.css";
 
 export default function Form() {
-    const [input, setInput] = useState()
+    const [input, setInput] = useState();
     const [isHidden, setIsHidden] = useState(true);
 
     function handleChange(event) {
         setInput(event.target.value);
-        event.target.value === "" ? setIsHidden(true) : setIsHidden(false)
+        event.target.value === "" ? setIsHidden(true) : setIsHidden(false);
         return;
     }
 
     return (
         <>
-            <input placeholder="Find a Duck..." onChange={handleChange}/>
+            <input
+                placeholder="Find a Duck..."
+                onChange={handleChange}
+                value={input}
+            />
             <p className={isHidden ? styles.hidden : ""}>You typed: {input} </p>
         </>
-    )
+    );
 }
